ad9cbc7f55432a7ee31201342c8944640578691c
synn <i@synn.me>
2019-04-19 21:40:29 +0800

    navbar refresh

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,31 +1,50 @@
 <template>
-    <div id="navbar">
+    <div id="navbar" @mouseover="mouseTip = true" @mouseleave="mouseTip = false">
         <div id="logo">
-            <img src="/logoCW.svg">
+            <img src="./static/logoCW.svg">
         </div>
         <div id="spacing"></div>
-        <ul>
+        <ul v-if="mouseTip">
             <li>首页</li>
             <li>新闻</li>
             <li>联系</li>
             <li>关于</li>
+        </ul>
+        <ul v-else>
+            <li>
+                <font-awesome-icon :icon="['fas', 'home']" size="lg" />
+            </li>
+            <li>
+                <font-awesome-icon :icon="['fas', 'feather']" size="lg" />
+            </li>
+            <li>
+                <font-awesome-icon :icon="['fas', 'comments']" size="lg" />
+            </li>
+            <li>
+                <font-awesome-icon :icon="['fas', 'seedling']" size="lg" />
+            </li>
         </ul>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'navbar'
+    name: 'navbar',
+    data() {
+        return {
+            mouseTip: false
+        }
+    }
 }
 </script>
 
 <style lang="less" scoped>
     #navbar {
         color: white;
-        height: 40px;
+        height: 60px;
         display: flex;
         flex-direction: row;
-        background: rgba(0, 0, 0, 0.6);
+        background: rgba(0, 0, 0, 0.8);
 
         #logo {
             display: flex;
@@ -35,7 +54,7 @@ export default {
             align-items: center;
 
             img {
-                height: 20px;
+                height: 40px;
                 flex-grow: 1;
             }
         }
@@ -60,7 +79,7 @@ export default {
             li {
                 min-width: 60px;
                 text-align: center;
-                line-height: 40px;
+                line-height: 60px;
                 flex-grow: 1;
                 align-items: center;
             }
